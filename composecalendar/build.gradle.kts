d5cc8c74b3810f799c693809a26d3eea0876d29c
Sean Proctor <sproctor@gmail.com>
2023-01-04 12:35:36 -0500

    release as 1.0.6

--- composecalendar/build.gradle.kts
+++ composecalendar/build.gradle.kts
@@ -86,7 +86,7 @@ val localProperties = Properties().apply {
 }
 
 group = localProperties.getProperty("group", "com.squaredem")
-version = "1.0.5"
+version = "1.0.6"
 
 val dokkaOutputDir = buildDir.resolve("dokka")
 
